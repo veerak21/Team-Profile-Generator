cc293656a20b25b59f7f7a142a149f87b759458f
C M Santosh Kumar <pagadala.kalyani@gmail.com>
2023-01-23 13:54:48 +0000

    Code added to all the classes in lib folder and commets added in the index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+//importing the employee roles
 const Manager = require("./lib/Manager.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
+
+//importing the modules
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
 const OUTPUT_DIR = path.resolve(__dirname, "dist");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const generateTeam = require("./src/template.js");
