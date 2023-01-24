dcc061960f86b23364389e72f1c752e8edd908ff
C M Santosh Kumar <pagadala.kalyani@gmail.com>
2023-01-24 13:49:20 +0000

    Renamed generated html file  and replaced in index file and HTML code added in the template file

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path = require("path");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "dist");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const outputPath = path.join(OUTPUT_DIR, "generated-team.html");
 const generateTeam = require("./src/template.js");
 
 teamArray = [];
